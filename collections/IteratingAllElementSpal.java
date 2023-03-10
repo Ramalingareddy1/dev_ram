1f820c30fc264ea730a282ddc6ddd0cdd8ba33c6
Ramalingareddy1 Chintakunta <ramareddyc1996@gmail.com>
2023-03-10 05:30:42 +0000

    iterate through all elements in a linked list starting at the specified position

---
+++ collections/IteratingAllElementSpal.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+  public class IteratingAllElementSpal {
+  public static void main(String[] args) {
+
+     LinkedList<String> l_list = new LinkedList<String>();
+   
+          l_list.add("Red");
+          l_list.add("Green");
+          l_list.add("Black");
+          l_list.add("White");
+          l_list.add("Pink");
+
+   Iterator p = l_list.listIterator(1);
+
+   // print list from second position
+   while (p.hasNext()) {
+   System.out.println(p.next());
+   }
+   }
+}
