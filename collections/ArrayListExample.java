9c58d380b4555db9b9ee6668d56fd816a9e8a6b1
Ramalingareddy1 Chintakunta <ramareddyc1996@gmail.com>
2023-03-10 04:25:13 +0000

    Adding ArrayListExample

---
+++ collections/ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        
+    
+    List<String> list_Strings= new ArrayList<String>();
+    list_Strings.add("Red");
+    list_Strings.add("Green"); 
+    list_Strings.add("Orange");
+    list_Strings.add("White");  
+    list_Strings.add("black");
+    System.out.println(list_Strings);
+    }
+}
