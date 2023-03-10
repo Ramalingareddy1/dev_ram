cadee3f5e82b18e23ee18e42b88920b4a8cbd8c9
Ramalingareddy1 Chintakunta <ramareddyc1996@gmail.com>
2023-03-10 05:18:24 +0000

    Appending liked list

---
+++ collections/AppendingLinkedList.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class AppendingLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> l_List= new LinkedList<String>();
+
+        l_List.add("Red");
+        l_List.add("Green");
+        l_List.add("White");
+        l_List.add("Orange");
+        l_List.add("Yellow");
+
+        System.out.println(" The list of linked list array elements is:"+l_List);
+
+
+
+
+    }
+    
+}
